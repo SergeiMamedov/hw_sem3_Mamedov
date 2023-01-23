3833ed526a809f7cf6baa7b67ffdb6fbcc826ed9
Sergei <sergeimamedov9976@gmail.com>
2023-01-24 02:07:42 +0600

    третья задача

---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int a = Prompt("Введите число: ");
+Math.Pow(a, 3);
+int index = 1;
+while (index <= a)
+{
+    double r = Math.Pow(index, 3);
+    System.Console.WriteLine($"Куб числа {index} от 1 до введенного равен: " + r);
+    index++;
+
+}
